66f4c0b33d349ceee10f20172e19c67029cb0435
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-23 13:11:03 +0300

    docs

---
+++ js/docs-setup.js
@@ -0,0 +1,153 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "translate",
+      "shortName": "translate",
+      "type": "overview",
+      "moduleName": "translate",
+      "shortDescription": "i18n library for angular",
+      "keywords": "angular api caching features i18n language library load loading overview parallel partional preffered translate translations"
+    },
+    {
+      "section": "api",
+      "id": "translate.$translate",
+      "shortName": "translate.$translate",
+      "type": "service",
+      "moduleName": "translate",
+      "shortDescription": "Service responsible for comunication with external code and manage other parts to work mutually",
+      "keywords": "$translate add addpart addparts api automatically call code comunication config corrupt current currentlang default equal error external force hashmap initialize key lang language library loaderservice loading manage merge method mutually name1 names options params parts pass passed refresh reload responsible return separate service set start storage storageservice string throw translate translation translations updated utils validateoptions values work"
+    },
+    {
+      "section": "api",
+      "id": "translate.cache",
+      "shortName": "translate.cache",
+      "type": "service",
+      "moduleName": "translate",
+      "shortDescription": "Service responsible for caching the translations data and prefered language in local storage, if it supported by browser",
+      "keywords": "$window api browser cache caching check data empty exists existsinlocalstorage getfromlocalstorage getlangfromcache getvaluesfromcache hashmap issupportlocalstorage key lang language langugage local localstorage merge method obj object passed prefered puttolocalstorage responsible result return service setlangtocache setvaluestocache storage string support supported translate translations undefined values"
+    },
+    {
+      "section": "api",
+      "id": "translate.directive:ngTranslate",
+      "shortName": "ngTranslate",
+      "type": "directive",
+      "moduleName": "translate",
+      "shortDescription": "Transform any html element to the translation container. ",
+      "keywords": "$translate api container directive element events html key ngtranslate service transform translate translation translations"
+    },
+    {
+      "section": "api",
+      "id": "translate.event",
+      "shortName": "translate.event",
+      "type": "object",
+      "moduleName": "translate",
+      "shortDescription": "Used to create event objects in translate.events",
+      "keywords": "api automatical call callback callbacks create data delete disposable event events generate generateid list method object objects pass publish registered return specific subscribe subscriber subscribers subscribtion subscription success token translate unique unsubscribe utils"
+    },
+    {
+      "section": "api",
+      "id": "translate.events",
+      "shortName": "translate.events",
+      "type": "service",
+      "moduleName": "translate",
+      "shortDescription": "Service responsible for providing access to library events. ",
+      "keywords": "$tranlslateevents access api callback console event events function implementation javascript langchanged library list log objects one-time provide providing register responsible service subscribe token token2 translate true unsibscribe utils var"
+    },
+    {
+      "section": "api",
+      "id": "translate.filter:translate",
+      "shortName": "translate",
+      "type": "filter",
+      "moduleName": "translate",
+      "shortDescription": "Transform translation key into translation",
+      "keywords": "$translate api filter key transform translate translate_expression translation"
+    },
+    {
+      "section": "api",
+      "id": "translate.loader",
+      "shortName": "translate.loader",
+      "type": "service",
+      "moduleName": "translate",
+      "shortDescription": "Service responsible for loading parts",
+      "keywords": "$http $q api complete data function loader loading loadpart method options parts passed promise received resolve resolved responsible return server service start transform transformation transformed translate"
+    },
+    {
+      "section": "api",
+      "id": "translate.loaderService",
+      "shortName": "translate.loaderService",
+      "type": "service",
+      "moduleName": "translate",
+      "shortDescription": "Service responsible for managing parts loading process",
+      "keywords": "$timeout $translate add addpart addparts api call callback called case check checkcounter config configuration configured count counter data delay equals event events fire force function functions hashmap increase increasecounter info initialization initialize isloaded isloading lang language languages list load loaded loader loaderservice loading loadingoff loadingon loadpart loadparts managing mark method mode names needed needload non-blocking object options pair pairs param part-lang parts preload prevented process progress receive received require reset resetcounter responsible return service skip start storage storagecallback succesful syncronization template time transformation translate true utils"
+    },
+    {
+      "section": "api",
+      "id": "translate.storage",
+      "shortName": "translate.storage",
+      "type": "service",
+      "moduleName": "translate",
+      "shortDescription": "Service responsible for storing the translations data while app is running",
+      "keywords": "api app check create data empty existing exists getvalue getvalues hashmap key lang language merge method object passed responsible return running service setvalues storage storing string translate translations values"
+    },
+    {
+      "section": "api",
+      "id": "translate.storageService",
+      "shortName": "translate.storageService",
+      "type": "service",
+      "moduleName": "translate",
+      "shortDescription": "Service responsible for managing storing and caching processes",
+      "keywords": "$translate api cache cachegetlang cachegettranslations cachesetlang cachesettranslations caching called changed choose config default disabled empty enabled event events exist fire getlang gettranslation gettranslations hashmap info initialization initialize key lang language load managing method object options process processes responsible return service setlang settranslations storage storagegetlang storagegettranslation storagegettranslations storageservice storagesetlang storagesettranslations storing string translate translation translations updated values work"
+    },
+    {
+      "section": "api",
+      "id": "translate.utils",
+      "shortName": "translate.utils",
+      "type": "service",
+      "moduleName": "translate",
+      "shortDescription": "Service provides some utility functions like errors, warnings and etc.",
+      "keywords": "$translateutils api changes data default directdatatransformation error errors errorthrow functions javascript library message method passed prefix received return service throw transformation transformed translate translations utility utils values warning warnings warningthrow write"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/bower_components/angular/angular.min.js",
+      "js/bower_components/angular-animate/angular-animate.min.js",
+      "js/bower_components/marked/marked.min.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "js/bower_components/angular/angular.min.js",
+    "js/bower_components/angular-animate/angular-animate.min.js",
+    "js/bower_components/marked/marked.min.js"
+  ]
+};
